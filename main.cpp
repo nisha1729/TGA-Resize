774205de6892cb5a4708f491bf6581696304cd7a
Nisha George <nishageorge1729@gmail.com>
2019-12-04 16:10:18 +0100

    Fixed warnings

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "halfsize.cpp"
 
 int main(int argc, char **argv)
+// int main()
 {
     tga p;
     char *in, *out;
     if (argc!=3)
         printf("\nInvalid input. Please try again.\n");
     else{        
-        in = argv[1];
-        out = argv[2];
+        in = argv[1];  //  File name of the original image
+        out = argv[2]; // File name of the resized image
         p.resizeTGA(in, out);
     }
     return 0;
